8471b13cf4885413bc926020c6f809e0b704a216
Caio <caio.dscamara@gmail.com>
2018-07-01 18:04:04 -0400

    T

--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  resource :github_webhooks, only: :create, defaults: { formats: :json }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
